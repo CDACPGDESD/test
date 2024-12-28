30c2a151fc0c96bfb8bdedccc702988d1cd04a99
CDACPGDESD <cdacdesd2024@gmail.com>
2024-12-28 17:20:10 +0530

    Revert " 6th commit"
    This reverts commit ffa86b30635b402fcb47b1d11a80256e5461b6ae.

--- hello.py
+++ hello.py
@@ -4,7 +4,10 @@ print("3rd commit")
 <<<<<<< HEAD
 print("4th commit")
 print("5th commit")
+<<<<<<< HEAD
 print("6th commit")
 =======
 printf("4th commit")
 >>>>>>> parent of 834b13d... updated
+=======
+>>>>>>> parent of ffa86b3...  6th commit
