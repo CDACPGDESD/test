d42073a1532ceeb1a0dbc64af5411bfd118e5cd3
CDACPGDESD <cdacdesd2024@gmail.com>
2024-12-28 17:18:01 +0530

    upadate

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 print("Hello, World!")
 print("2nd commit")
 print("3rd commit")
+<<<<<<< HEAD
 print("4th commit")
 print("5th commit")
 print("6th commit")
+=======
+printf("4th commit")
+>>>>>>> parent of 834b13d... updated
