d921e4d6cd9ffe5cb1455e59af6af7dc69eb31f4
CDACPGDESD <cdacdesd2024@gmail.com>
2024-12-28 17:21:36 +0530

    update 6

--- hello.py
+++ hello.py
@@ -1,13 +1,7 @@
 print("Hello, World!")
 print("2nd commit")
 print("3rd commit")
-<<<<<<< HEAD
 print("4th commit")
 print("5th commit")
-<<<<<<< HEAD
 print("6th commit")
-=======
 printf("4th commit")
->>>>>>> parent of 834b13d... updated
-=======
->>>>>>> parent of ffa86b3...  6th commit
